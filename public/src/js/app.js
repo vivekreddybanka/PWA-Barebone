29b09e0a2188f4a5008bbd18a14f64fdb5394ca9
vivek.banka <vivek.banka@ies-co.com>
2022-03-14 15:10:16 -0500

    Added the Promise.js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,5 +1,11 @@
+var button = document.querySelector('#start_button');
+var output = document.querySelector('#output');
+
 var deferrendinstall
 
+if  (!window.Promise) {
+    window.Promise = Promise
+}
 if('serviceWorker' in navigator) {
     navigator.serviceWorker
     .register('/sw.js')
@@ -7,6 +13,22 @@ if('serviceWorker' in navigator) {
         console.log('register the service worker');
     });
 }
+button.addEventListener('click', function() {
+    var promise = new Promise(function(resolve, reject) {
+        setTimeout(function() {
+            resolve('https:/swapi.co/api/people/1');
+        }, 3000);
+    })
+    .then(function(url) {
+        return fetch(url);
+    })
+    .then(function(response) {
+        return response.json();
+    })
+    .then(function(data) {
+        output.textContent = data.name;
+    })
+})
 
 window.addEventListener('beforeinstallprompt', function(event) {
     console.log('before install prompt fired');
